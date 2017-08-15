6b2b4b8334e1da5f790d1e7685747f3ba1e77299
Hugh Morrison <hmorri32@gmail.com>
2017-08-15 17:36:52 -0600

    implement luhn alg, slight refactor

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,13 +1,42 @@
 card_number = "4929735477250543"
 
+def integers(arr)
+  arr.map(&:to_i)
+end
+
+def times_two(arr)
+  arr.map.with_index {|num, i| i.even? ? num * 2 : num }
+end
+
+def add_over_ten(arr)
+  arr.map { |num| num >= 10 ? num = 1 + (num - 10) : num }
+end
+
+def sum_all_digits(arr)
+  arr.reduce(:+)
+end
+
 def validate(numbers)
-  total = numbers.split("")
-                 .map(&:to_i)
-                 .map.with_index { |num, i| i.even? ? num * 2 : num }
-                 .map { |num| num >= 10 ? num = 1 + (num - 10) : num }
-                 .reduce(:+) 
-                 
+  split      = numbers.split("")
+  int        = integers(split)
+  doubled    = times_two(int)
+  added      = add_over_ten(doubled)
+  total      = sum_all_digits(added)
   total % 10 == 0 ? "ultra valid" : "def not"
 end
+
+
+
+
+# def validate(numbers)
+#   total = numbers.split("")
+#                  .map(&:to_i)
+#                  .map.with_index { |num, i| i.even? ? num * 2 : num }
+#                  .map { |num| num >= 10 ? num = 1 + (num - 10) : num }
+#                  .reduce(:+) 
+                 
+#   puts total % 10 == 0 ? "ultra valid" : "def not"
+# end
+
 
 validate(card_number)
