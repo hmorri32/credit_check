e56ae23fa6d5e3c94c2b673f1d22bdfe9b392355
Hugh Morrison <hmorri32@gmail.com>
2017-08-16 15:26:05 -0600

    build credit_methods class. write unit tests for each method

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,42 +1,21 @@
+require_relative 'credit_methods'
+
 class CreditCheck
   attr_reader :valid
-  
+
   def initialize(cc_number)
     @cc_number = cc_number
     @valid     = false
-  end
-
-  def splitter(string)
-    string.split("")
-  end
- 
-  def integers(arr)
-    arr.map(&:to_i)
-  end
-
-  def reverse_it(arr)
-    arr.reverse
-  end
-
-  def every_other_times_two(arr)
-    arr.map.with_index {|num, i| i.odd? ? num * 2 : num }
-  end
-
-  def add_over_ten(arr)
-    arr.map { |num| num >= 10 ? num - 9  : num }
-  end
-
-  def add_all_digits(arr)
-    arr.reduce(:+)
+    @methods   = CreditMethods.new
   end
 
   def validate
-    split           = splitter(@cc_number)
-    as_ints         = integers(split)
-    reversed        = reverse_it(as_ints)
-    by_two          = every_other_times_two(reversed)
-    added_over_ten  = add_over_ten(by_two)
-    total           = add_all_digits(added_over_ten)
+    split           = @methods.splitter(@cc_number)
+    as_ints         = @methods.integers(split)
+    reversed        = @methods.reverse_it(as_ints)
+    by_two          = @methods.every_other_times_two(reversed)
+    added_over_ten  = @methods.add_over_ten(by_two)
+    total           = @methods.add_all_digits(added_over_ten)
     puts total % 10 == 0 ? @valid = true : @valid
   end
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -1,48 +1,50 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/credit_check'
+require './lib/credit_methods'
 
 class UnitTests < Minitest::Test
-
   def setup
     @card         = CreditCheck.new("4929735477250543")
     @valid_amex   = CreditCheck.new("342804633855673")
     @invalid_amex = CreditCheck.new("342801633855673")
+    @methods      = CreditMethods.new
   end
   
   def test_card_existence
     assert_instance_of CreditCheck, @card
     assert_instance_of CreditCheck, @valid_amex
     assert_instance_of CreditCheck, @invalid_amex
+    assert_instance_of CreditMethods, @methods
   end
 
   def test_splitter
-    split = @card.splitter("4929735477250543")
+    split = @methods.splitter("4929735477250543")
     assert_equal ["4", "9", "2", "9", "7", "3", "5", "4", "7", "7", "2", "5", "0", "5", "4", "3"], split
   end
 
   def test_integers
-    ints = @card.integers(["1", "2", "3"])
+    ints = @methods.integers(["1", "2", "3"])
     assert_equal [1,2,3], ints 
   end
 
   def test_reverse_method
-    reversed = @card.reverse_it([1, 2, 3])
+    reversed = @methods.reverse_it([1, 2, 3])
     assert_equal [3, 2, 1], reversed
   end
 
   def test_every_other_doubler
-    doubled = @card.every_other_times_two([1, 2, 3, 4, 5])
+    doubled = @methods.every_other_times_two([1, 2, 3, 4, 5])
     assert_equal [1, 4, 3, 8, 5], doubled
   end
 
   def test_add_over_ten
-    summed = @card.add_over_ten([12, 2, 13])
+    summed = @methods.add_over_ten([12, 2, 13])
     assert_equal [3, 2, 4], summed
   end
 
   def test_add_all_digits 
-    sum = @card.add_all_digits([1, 2, 3, 4, 5, 6])
+    sum = @methods.add_all_digits([1, 2, 3, 4, 5, 6])
     assert_equal 21, sum 
   end
 end
